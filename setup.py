9d454518ebe12f93730cda61b8aab010518f1d12
Neville Yee <neville.yee@alumni.york.ac.uk>
2021-06-11 13:53:39 +0100

    Added requirements and setup scripts

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+Copyright (C) 2021 Rosalind Franklin Institute
+
+This code is distributed under the ApacheV2 license
+"""
+from skbuild import setup
+
+def main():
+    """
+    Setup the package
+    """
+
+    setup(
+        package_dir={"": "src"},
+        packages=["Ot2Rec"],
+        install_requires=[],
+        entry_points={
+            "console_scripts": [
+                "o2r.new=Ot2Rec.main:new_proj",
+            ]
+        }
+    )
+
+if __name__ == '__main__':
+    main()
