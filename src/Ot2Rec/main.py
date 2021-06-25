ba7efc83b792762c6c237674663863f66286a79c
Neville Yee <neville.yee@alumni.york.ac.uk>
2021-06-25 15:57:05 +0000

    Added method to create yaml config file for alignment in main.

--- src/Ot2Rec/main.py
+++ src/Ot2Rec/main.py
@@ -193,7 +193,7 @@ def update_ctffind_yaml():
         mc2_md = pd.DataFrame(yaml.load(f, Loader=yaml.FullLoader))[['ts', 'angles']]
 
     # Read in previous ctffind output metadata (as Pandas dataframe) for old projects
-    ctf_md_name = project_name + '_ctf_mdout.yaml'
+    ctf_md_name = project_name + '_ctffind_mdout.yaml'
     if os.path.isfile(ctf_md_name):
         is_old_project = True
         with open(ctf_md_name, 'r') as f:
@@ -275,4 +275,68 @@ def run_ctffind():
         ctffind_obj.run_ctffind()
 
 
+def update_align_yaml():
+    """
+    Subroutine to update yaml file for IMOD newstack / alignment
+    """
 
+    project_name = get_proj_name()
+    
+    # Check if align and motioncorr yaml files exist
+    align_yaml_name = project_name + '_align.yaml'
+    mc2_yaml_name = project_name + '_mc2.yaml'
+    if not os.path.isfile(align_yaml_name):
+        raise IOError("Error in Ot2Rec.main.update_align_yaml: alignment config file not found.")
+    if not os.path.isfile(mc2_yaml_name):
+        raise IOError("Error in Ot2Rec.main.update_align_yaml: motioncorr config file not found.")
+
+    # Read in MC2 metadata (as Pandas dataframe)
+    # We only need the TS number and the tilt angle for comparisons at this stage
+    mc2_md_name = project_name + '_mc2_mdout.yaml'
+    with open(mc2_md_name, 'r') as f:
+        mc2_md = pd.DataFrame(yaml.load(f, Loader=yaml.FullLoader))[['ts', 'angles']]
+
+    # Read in previous alignment output metadata (as Pandas dataframe) for old projects
+    align_md_name = project_name + '_align_mdout.yaml'
+    if os.path.isfile(align_md_name):
+        is_old_project = True
+        with open(align_md_name, 'r') as f:
+            align_md = pd.DataFrame(yaml.load(f, Loader=yaml.FullLoader))[['ts', 'angles']]
+    else:
+        is_old_project = False
+
+    # Diff the two dataframes to get numbers of tilt-series with unprocessed data
+    if is_old_project:
+        merged_md = mc2_md.merge(align_md,
+                                 how='outer',
+                                 indicator=True)
+        unprocessed_images = merged_md.loc[lambda x: x['_merge']=='left_only']
+    else:
+        unprocessed_images = mc2_md
+
+    unique_ts_numbers = unprocessed_images['ts'].sort_values(ascending=True).unique().tolist()
+
+    # Read in ctffind yaml file, modify, and update
+    # read in MC2 yaml as well (some parameters depend on MC2 settings)
+    align_params = prmMod.read_yaml(project_name=project_name,
+                                    filename=align_yaml_name)
+    mc2_params = prmMod.read_yaml(project_name=project_name,
+                                  filename=mc2_yaml_name)
+    
+    align_params.params['System']['process_list'] = unique_ts_numbers
+    align_params.params['BatchRunTomo']['pixel_size'] = mc2_params.params['MC2']['desired_pixel_size']
+    
+    with open(align_yaml_name, 'w') as f:
+        yaml.dump(align_params.params, f, indent=4, sort_keys=False) 
+
+        
+def create_align_yaml():
+    """
+    Subroutine to create new yaml file for IMOD newstack / alignment
+    """
+
+    project_name = get_proj_name()
+    
+    # Create the yaml file, then automatically update it
+    prmMod.new_align_yaml(project_name)
+    update_align_yaml()
