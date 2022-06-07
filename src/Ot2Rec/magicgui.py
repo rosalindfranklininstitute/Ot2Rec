e522ce98de2478568e32618913243dc0f22f9b50
Neville Yee <neville.yee@alumni.york.ac.uk>
2022-06-07 12:51:09 +0000

    Added MGUI utility for O2R-MC2

--- src/Ot2Rec/magicgui.py
+++ src/Ot2Rec/magicgui.py
@@ -55,18 +55,93 @@ def get_args_new_proj(
     Function to add arguments to parser for new project
 
     ARGS:
-    project_name (str) :: Name of current project
-    source_folder (str) :: Path to folder with raw images (Default: ../raw/)
-    folder_prefix (str) :: Common prefix of raw tilt series folder(s)
-    file_prefix (str) :: Common prefix of raw image files (Default: project
-    ext (str) :: Extension of raw image files (Default: mrc)
-    stack_field (int) :: Field number of tilt series indices (Default: 0)
-    index_field (int) :: Field number of image indices (Default: 1)
+    project_name (str)    :: Name of current project
+    source_folder (str)   :: Path to folder with raw images (Default: ../raw/)
+    folder_prefix (str)   :: Common prefix of raw tilt series folder(s)
+    file_prefix (str)     :: Common prefix of raw image files (Default: project)
+    ext (str)             :: Extension of raw image files (Default: mrc)
+    stack_field (int)     :: Field number of tilt series indices (Default: 0)
+    index_field (int)     :: Field number of image indices (Default: 1)
     tiltangle_field (int) :: Field number of tilt angles (Default: 2)
-    no_mdoc (bool) :: True if no MDOC file provided (Default: False)
+    no_mdoc (bool)        :: True if no MDOC file provided (Default: False)
 
     OUTPUTs:
     Namespace
     """
 
+    return locals()
+
+
+@mg(
+    call_button="Create config file",
+    layout="vertical",
+    result_widget=False,
+
+    project_name={"label": "Project name *"},
+    pixel_size={"label": "Pixel size (A) *"},
+    output_folder={"label": "MC2 output folder"},
+    file_prefix={"label": "File prefix (if different from project name)"},
+    jobs_per_gpu={"label": "Jobs per GPU",
+                  "min": 1},
+    gpu_mem_usage={"label": "GPU memory usage (if applicable)",
+                   "widget_type": "FloatSlider",
+                   "min": 0.1,
+                   "max": 2.0},
+    gain={"label": "Gain reference file (if applicable)",
+          "widget_type": "FileEdit",
+          "mode": "w"},
+    super_res={"label": "Super-resolution images?"},
+    discard_top={"label": "# Frames discarded FROM TOP of images",
+                 "min": 0},
+    discard_bottom={"label": "# Frames discarded FROM BOTTOM of images",
+                    "min": 0},
+    tolerance={"label": "Alignment error threshold (in pixels)",
+               "min": 0},
+    max_iter={"label": "Maximum MC2 iterations",
+              "min": 1},
+    patch_size={"label": "Patch configurations (Nx, Ny, %overlap)"},
+    no_subgroups={"label": "Do NOT use subgroups in alignments"}
+)
+def get_args_mc2(
+        project_name="",
+        pixel_size=None,
+        output_folder=Path("./motioncor/"),
+        file_prefix="",
+        no_gpu=False,
+        jobs_per_gpu=2,
+        gpu_mem_usage=1.0,
+        exec_path=Path("/opt/lmod/modules/motioncor2/1.4.0/MotionCor2_1.4.0/MotionCor2_1.4.0_Cuda110"),
+        gain="",
+        super_res=False,
+        discard_top=0,
+        discard_bottom=0,
+        tolerance=0.5,
+        max_iter=10,
+        patch_size=[5,5,20],
+        no_subgroups=False,
+):
+    """
+    Function to add arguments to parser for MotionCor
+
+    ARGS:
+    project_name (str) :: Name of current project
+    pixel_size (float) :: Image pixel size in Angstroms
+    output_folder (str) :: Path to folder for storing motion-corrected images (Default: ./motioncor/)
+    file_prefix (str)     :: Common prefix of raw image files (Default: project)
+    no_gpu (bool) :: Use CPU only for motion-correction
+    jobs_per_gpu (int) :: Number of job instance(s) per GPU
+    gpu_mem_usage (float) :: MotionCor2 GPU memory usage
+    exec_path (str) :: Path to MotionCor2 executable
+    gain (str) :: Path to gain reference file (leave blank if none)
+    super_res (bool) :: True if super-resolution images used
+    discard_top (int) :: Number of frames discarded from top per image
+    discard_bottom (int) :: Number of frames discarded from bottom per image
+    tolerance (float) :: Threshold of alignment errors in pixels
+    max_iter (int) :: Maximum number of iterations performed by MotionCor2
+    patch_size (int) :: Size of patches used in alignment
+    no_subgroups (bool) :: Do not use subgroups in alignment
+
+    OUTPUTs:
+    Namespace
+    """
     return locals()
