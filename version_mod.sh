dced0c2d618a8517415a98d289259278c3cd9bb8
Neville Yee <neville.yee@alumni.york.ac.uk>
2022-10-06 10:06:22 +0000

    Removed unwanted space in version_mod script output (in setup.py).

--- version_mod.sh
+++ version_mod.sh
@@ -7,7 +7,7 @@ sed -i -r 's/(^s*version:) ([0-9.]+)/\1 '$version'/' construct.yaml
 sed -i -r 's/(- Ot2Rec) ([0-9.]+)/\1 '$version'/' construct.yaml
 
 # Change version number in setup.py
-sed -i -r "s/(version=)('[0-9.]+')/\1 \'"$version"\'/" setup.py
+sed -i -r "s/(version=)('[0-9.]+')/\1'"$version"'/" setup.py
 
 # Change version number in conda-build/meta.py
 sed -i -r 's/(version:) ([0-9.]+)/\1 '$version'/' conda-build/meta.yaml
