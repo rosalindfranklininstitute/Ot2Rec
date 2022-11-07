9afc00b1f70f8b5e74be333e142c26ee8bc7b429
elainehoml <elaine.ho@rfi.ac.uk>
2022-11-07 18:15:22 +0000

    fix imod stack creation

--- src/Ot2Rec/aretomo.py
+++ src/Ot2Rec/aretomo.py
@@ -232,11 +232,11 @@ def _update_volz(args, aretomo_params):
 def _create_stacks_with_imod(args):
     # Uses align to create the InMrc and AngFile in correct form
         try:
-            align.create_yaml([
-                args["project_name"],
-                str(args["rot_angle"]),
-                '-o',
-                args["output_path"]])
+            args_in_align = mgMod.get_args_align
+            args_in_align.project_name.value = args["project_name"]
+            args_in_align.rot_angle.value = args["rot_angle"]
+            args_in_align.output_folder.value = args["output_path"]
+            align.create_yaml(args_in=args_in_align)
             align.run(
                 newstack=True,
                 do_align=False,
