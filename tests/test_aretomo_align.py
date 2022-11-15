f87e4ba71764181a1b3b9b0e30143b1ecf5338af
elainehoml <elaine.ho@rfi.ac.uk>
2022-11-15 11:26:56 +0000

    yaml creation and imod stack tests

---
+++ tests/test_aretomo_align.py
@@ -0,0 +1,128 @@
+# Copyright 2022 Rosalind Franklin Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import os
+import shutil
+import tempfile
+import unittest
+from unittest.mock import patch
+
+import mrcfile
+import numpy as np
+from Ot2Rec import aretomo
+from Ot2Rec import logger as logMod
+from Ot2Rec import magicgui as mgMod
+from Ot2Rec import params as prmMod
+
+
+class AreTomoAlignSmokeTest(unittest.TestCase):
+
+    def _create_expected_input_args(self):
+        """Create expected input magicGUI args
+        NOTE: This is not the proper way to write this function,
+        please refer to the savu tests instead. This returns the args as
+        a dict, which the aretomo plugin uses as input, rather than
+        the FunctionGui. This will be made consistent in the future"""
+        args = mgMod.get_args_aretomo(
+            project_name="TS",
+            aretomo_mode=0,
+            pixel_size=1.0,
+            input_mrc_folder="./motioncor",
+            output_path="./aretomo_aligned",
+        )
+
+        return args
+
+    def _create_expected_folder_structure(self):
+        tmpdir = tempfile.TemporaryDirectory()
+        template_folder = f"{os.path.dirname(__file__)}/template_yamls"
+
+        # create motioncor yaml file
+        mc_yaml = f"{tmpdir.name}/TS_mc2.yaml"
+        shutil.copyfile(f"{template_folder}/TS_mc2.yaml", mc_yaml)
+
+        # create motioncor mdout yaml file
+        mc_md_yaml = f"{tmpdir.name}/TS_mc2_mdout.yaml"
+        shutil.copyfile(f"{template_folder}/TS_mc2_mdout.yaml", mc_md_yaml)
+
+        # create motioncor folder input
+        os.mkdir(f"{tmpdir.name}/motioncor")
+        tas = [-30.0, 0.0, 30.0]
+        mc_mrcs = [
+            f"{tmpdir.name}/motioncor/TS_0001_{ang}.mrc" for ang in tas
+        ]
+        for mc_mrc in mc_mrcs:
+            with mrcfile.new(mc_mrc) as mrc:
+                mrc.set_data(np.arange(9, dtype=np.int8).reshape(3,3))
+
+        return tmpdir
+
+    def _create_expected_st_folder_structure(self, tmpdir):
+        """Create folder structure expected from imod stacks"""
+        template_folder = f"{os.path.dirname(__file__)}/template_yamls"
+        # create align.yaml file
+        align_yaml = f"{tmpdir.name}/TS_align.yaml"
+        shutil.copyfile(f"{template_folder}/TS_align.yaml", align_yaml)
+
+        # create align_mdout.yaml file
+        align_md_yaml = f"{tmpdir.name}/TS_align_mdout.yaml"
+        shutil.copyfile(f"{template_folder}/TS_align_mdout.yaml", align_md_yaml)
+
+        # create st files
+        os.mkdir(f"{tmpdir.name}/aretomo_aligned")
+        os.mkdir(f"{tmpdir.name}/aretomo_aligned/TS_0001")
+        st_file = f"{tmpdir.name}/aretomo_aligned/TS_0001/TS_0001.st"
+        with mrcfile.new(st_file) as mrc:
+            mrc.set_data(np.arange(18, dtype=np.int8).reshape(2,3,3))
+
+        # create rawtlt files
+        rawtlt_file = f"{tmpdir.name}/aretomo_aligned/TS_0001/TS_0001.rawtlt"
+        with open(rawtlt_file, "w") as f:
+            f.write("-30.0\n")
+            f.write("0.0\n")
+            f.write("30.0\n")
+
+    @patch("Ot2Rec.align.run")
+    def test_imod_stack_creation(self, imod_st_mock):
+        """Test imod stack creation is called with motioncor expected input"""
+        # Create expected input
+        tmpdir = self._create_expected_folder_structure()
+        os.chdir(tmpdir.name)
+        args = self._create_expected_input_args()
+
+        # Create yaml
+        aretomo.create_yaml(args)
+
+        # Test that imod stack creation is called
+        self.assertTrue(imod_st_mock.called)
+
+    @patch("Ot2Rec.align.run")
+    def test_yaml_creation(self, imod_st_mock):
+        # Create expected input until after stacks are created
+        tmpdir = self._create_expected_folder_structure()
+        os.chdir(tmpdir.name)
+        args = self._create_expected_input_args()
+        self._create_expected_st_folder_structure(tmpdir)
+
+        # Create yaml
+        aretomo.create_yaml(args)
+
+        # Read params
+        params = prmMod.read_yaml(
+            project_name="TS",
+            filename="./TS_aretomo_align.yaml",
+        )
+
+        # Ensure process list is not empty
+        self.assertNotEqual(len(params.params["System"]["process_list"]), 0)
